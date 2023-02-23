30861bfab2a38dd5cb3ac72307469302b024a7fa
sanshrav1311 <92669335+sanshrav1311@users.noreply.github.com>
2023-02-23 22:23:49 +0530

    linked proposal creater "Create a proposal"

--- scripts/helper/intro.js
+++ scripts/helper/intro.js
@@ -1,28 +1,29 @@
-const prompts = require("prompts")
-const { handleDAOCreation } = require("./daoCreation")
+const prompts = require("prompts");
+const { handleDAOCreation } = require("./daoCreation");
+const { proposalCreation } = require("./createProposal");
 
 const initialQuestions = [
-    {
-        type: "text",
-        name: "name",
-        message: "What's your name?",
-    },
-    {
-        type: "autocomplete",
-        name: "value",
-        message: "What do you want to do?",
-        choices: [
-            { title: "Create a DAO", value: handleDAOCreation },
-            { title: "Create a proposal", value: "createProposal" },
-            { title: "Vote on a proposal", value: "voteProposal" },
-            { title: "Add a member", value: "addMember" },
-        ],
-    },
-]
+  {
+    type: "text",
+    name: "name",
+    message: "What's your name?",
+  },
+  {
+    type: "autocomplete",
+    name: "value",
+    message: "What do you want to do?",
+    choices: [
+      { title: "Create a DAO", value: handleDAOCreation },
+      { title: "Create a proposal", value: proposalCreation },
+      { title: "Vote on a proposal", value: "voteProposal" },
+      { title: "Add a member", value: "addMember" },
+    ],
+  },
+];
 
-const exitHandler = () => console.log("") || true
+const exitHandler = () => console.log("") || true;
 
 module.exports.introHandler = async () => {
-    const response = await prompts(initialQuestions, { onCancel: exitHandler })
-    console.log(await response.value())
-}
+  const response = await prompts(initialQuestions, { onCancel: exitHandler });
+  console.log(await response.value());
+};
