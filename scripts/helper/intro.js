adf5603a3ac62a45e717ed7a38195626b82dd390
sanshrav1311 <92669335+sanshrav1311@users.noreply.github.com>
2023-02-25 14:53:47 +0530

    Updated intro.js to create a new wallet

--- scripts/helper/intro.js
+++ scripts/helper/intro.js
@@ -1,6 +1,10 @@
 const prompts = require("prompts");
-const { handleDAOCreation } = require("./daoCreation");
-const { proposalCreation } = require("./createProposal");
+const { ethers } = require("ethers");
+const crypto = require("crypto");
+const { afterQuestions } = require("./afterQuestions.js");
+// const { handleDAOCreation } = require("./daoCreation");
+// const { proposalCreation } = require("./createProposal");
+// const { voteProposal } = require("./voteProposal");
 
 const initialQuestions = [
   {
@@ -9,21 +13,32 @@ const initialQuestions = [
     message: "What's your name?",
   },
   {
-    type: "autocomplete",
-    name: "value",
-    message: "What do you want to do?",
-    choices: [
-      { title: "Create a DAO", value: handleDAOCreation },
-      { title: "Create a proposal", value: proposalCreation },
-      { title: "Vote on a proposal", value: "voteProposal" },
-      { title: "Add a member", value: "addMember" },
-    ],
+    type: "text",
+    name: "password",
+    message: "enter password",
   },
+  // {
+  //   type: "autocomplete",
+  //   name: "value",
+  //   message: "What do you want to do?",
+  //   choices: [
+  //     { title: "Create a DAO", value: handleDAOCreation },
+  //     { title: "Create a proposal", value: proposalCreation },
+  //     { title: "Vote on a proposal", value: voteProposal },
+  //     { title: "Add a member", value: "addMember" },
+  //   ],
+  // },
 ];
 
 const exitHandler = () => console.log("") || true;
 
 module.exports.introHandler = async () => {
   const response = await prompts(initialQuestions, { onCancel: exitHandler });
-  console.log(await response.value());
+  // console.log(response.name + response.password);
+  const sha256Hasher = crypto.createHmac("sha256", response.password);
+  const priv_key = sha256Hasher.update(response.name).digest("hex");
+  // console.log(priv_key);
+  const wallet = new ethers.Wallet(priv_key);
+  console.log(wallet.address);
+  await afterQuestions();
 };
