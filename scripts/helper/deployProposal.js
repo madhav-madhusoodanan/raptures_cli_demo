0488e044f44b8686182e8a0ee1e94469eaf14670
sanshrav1311 <92669335+sanshrav1311@users.noreply.github.com>
2023-03-01 16:27:38 +0530

    Update deployProposal.js

--- scripts/helper/deployProposal.js
+++ scripts/helper/deployProposal.js
@@ -9,9 +9,11 @@ async function main() {
     "Proposal",
     proposal.address
   );
+  // console.log(contractProposal.address);
+  // console.log(await contractProposal.getProposalCount());
+  // await contractProposal.createProposal("apple", "ball");
+  // console.log(await contractProposal.getProposal(0));
 }
-
-module.exports.contractProposal = this.contractProposal;
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
@@ -19,3 +21,5 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+module.exports.contractProposal = this.contractProposal;
