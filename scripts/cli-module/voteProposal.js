514758b34cd2f3f6db0a6056424e085ede30e866
sanshrav1311 <92669335+sanshrav1311@users.noreply.github.com>
2023-03-02 13:23:15 +0530

    Update voteProposal.js

--- scripts/cli-module/voteProposal.js
+++ scripts/cli-module/voteProposal.js
@@ -1,15 +1,6 @@
 const prompts = require("prompts");
 
 var proposalArray = [];
-// async function getProposals() {
-//     contractProposal.createProposal("aple", "bal")
-//     const proposalCount = await contractProposal.getProposalCount()
-//     proposalArray = []
-//     for (let i = 0; i < proposalCount; i++) {
-//         let proposal = await contractProposal.getProposal(i)
-//         if (proposal.isClosed) proposalArray.push({ title: proposal.title })
-//     }
-// }
 
 const voteOnProposal = [
   {
@@ -17,9 +8,6 @@ const voteOnProposal = [
     name: "vote",
     message: "for which proposal you want to vote for?",
     choices: proposalArray,
-    //   for(proposal in proposalArray){
-    //     { title: proposal.title, value: "vote" },
-    // }
   },
 ];
 
