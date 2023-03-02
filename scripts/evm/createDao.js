67794a5c61b3c57efc321a253145ec810fa82b3d
sanshrav1311 <92669335+sanshrav1311@users.noreply.github.com>
2023-03-02 10:23:53 +0530

    Update createDao.js

--- scripts/evm/createDao.js
+++ scripts/evm/createDao.js
@@ -1,7 +1,14 @@
-async function createDAO(config){
-    // config.name, config, contract, ...arguments.
+const hre = require("hardhat");
+const { introHandler } = require("../cli-module/intro");
 
-    // TODO: create dao
-    const dao = null
-    return dao
+async function createDAO(config) {
+  // config.name, config, contract, ...arguments.
+  const daoFactory = await hre.ethers.getContractFactory("Raptures");
+  const Dao = await daoFactory.deploy(config.token, config.timeLock);
+
+  // TODO: create dao
+  const dao = Dao.connect(introHandler(functionObject));
+  return dao;
 }
+
+module.exports = createDAO;
