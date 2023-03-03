0645c256b245267b632b6c68281f0f10319e8cb2
sanshrav1311 <92669335+sanshrav1311@users.noreply.github.com>
2023-03-03 17:23:24 +0530

    Update dao.js

--- scripts/dao.js
+++ scripts/dao.js
@@ -1,7 +1,11 @@
-const hre = require("hardhat")
-const { introHandler } = require("./cli-module/intro")
-const { afterQuestions } = require("./cli-module/afterQuestions.js")
-const { createWallet } = require("./evm/createWallet")
+const hre = require("hardhat");
+const { introHandler } = require("./cli-module/intro");
+const { afterQuestions } = require("./cli-module/afterQuestions.js");
+const { createWallet } = require("./evm/createWallet");
+const { createDAO } = require("./evm/createDao");
+const { handleDAOCreation } = require("./cli-module/daoCreation");
+const { proposalCreation } = require("./cli-module/createProposal");
+const { voteProposal } = require("./cli-module/voteProposal");
 
 /* const response = await prompts({
     type: "number",
@@ -11,37 +15,37 @@ const { createWallet } = require("./evm/createWallet")
 }) */
 
 async function main() {
-    /* 
+  /* 
     handle solana or evm logic here
   */
 
-    const functionObject = {
-        createWallet,
-        handleDAOCreation,
-        proposalCreation,
-        voteProposal,
-        temp: {},
-    }
+  const functionObject = {
+    createWallet,
+    handleDAOCreation,
+    proposalCreation,
+    voteProposal,
+    temp: {},
+  };
 
-    const wallet = await introHandler(functionObject)
-    const nextActionFunction = await afterQuestions()
-    const actionResponse = await nextActionFunction()
-
-    /* 
-      actionResponse will have the set of options that the user chose
-      along with the associated function executor
-      
-      this executor is blockchain dependent
-
-      actionResponse  = {
-        func,
-        [name, addresss, etc, ...arguments]
-      }
-    */
-    const response = actionResponse.func(actionResponse)
+  const wallet = await introHandler(functionObject);
+  const nextActionFunction = await afterQuestions();
+  const actionResponse = await nextActionFunction();
+  /* 
+  actionResponse will have the set of options that the user chose
+  along with the associated function executor
+  
+  this executor is blockchain dependent
+  
+  actionResponse  = {
+    func,
+    [name, addresss, etc, ...arguments]
+  }
+  */
+  console.log(actionResponse);
+  const response = actionResponse.value.func(actionResponse);
 }
 
 main().catch((error) => {
-    console.error(error)
-    process.exitCode = 1
-})
+  console.error(error);
+  process.exitCode = 1;
+});
