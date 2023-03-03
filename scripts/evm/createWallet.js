222e565a65ceae700095bbbda4c062d5e8054dc4
sanshrav1311 <92669335+sanshrav1311@users.noreply.github.com>
2023-03-03 17:24:43 +0530

    Update createWallet.js

--- scripts/evm/createWallet.js
+++ scripts/evm/createWallet.js
@@ -1,13 +1,19 @@
-function sha256Hash(public_key, secret_key) {
-    const sha256Hasher = crypto.createHmac("sha256", secret_key);
-    const priv_key = sha256Hasher.update(public_key).digest("hex");
-    return priv_key;
-  }
+const { crypto } = require("crypto");
 
-async function createWallet(response) {
-    const priv_key = sha256Hash(response.name, response.password)
-    const wallet = new ethers.Wallet(priv_key)
-    return wallet
+function sha256Hash(public_key, secret_key) {
+  const sha256Hasher = crypto.createHmac("sha256", secret_key);
+  const priv_key = sha256Hasher.update(public_key).digest("hex");
+  return priv_key;
 }
 
-module.exports = createWallet
+// async function createWallet(response) {
+//   const priv_key = sha256Hash(response.name, response.password);
+//   const wallet = new ethers.Wallet(priv_key);
+//   return wallet;
+// }
+
+module.exports.createWallet = (response) => {
+  const priv_key = sha256Hash(response.name, response.password);
+  const wallet = new ethers.Wallet(priv_key);
+  return wallet;
+};
