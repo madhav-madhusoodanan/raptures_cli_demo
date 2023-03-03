cec69792bb477b45066bbe3c098406971fe14ff5
sanshrav1311 <92669335+sanshrav1311@users.noreply.github.com>
2023-03-03 17:25:57 +0530

    Update daoCreation.js

--- scripts/cli-module/daoCreation.js
+++ scripts/cli-module/daoCreation.js
@@ -1,18 +1,24 @@
-const prompts = require("prompts")
+const prompts = require("prompts");
+const { createDAO } = require("../evm/createDao");
 
 module.exports.handleDAOCreation = async () => {
-    const daoPrompt = [
+  const daoPrompt = [
+    {
+      type: "autocomplete",
+      name: "value",
+      message: "Do you want to use a template or create one from scratch?",
+      choices: [
         {
-            type: "autocomplete",
-            name: "value",
-            message:
-                "Do you want to use a template or create one from scratch?",
-            choices: [
-                { title: "Use a template" },
-                { title: "Create from scratch" },
-            ],
+          title: "Use a template",
+          value: { func: createDAO /*arguments*/ },
         },
-    ]
-    const response = await prompts(daoPrompt)
-    return response
-}
+        {
+          title: "Create from scratch",
+          value: { func: createDAO /*arguments*/ },
+        },
+      ],
+    },
+  ];
+  const response = await prompts(daoPrompt);
+  return response;
+};
