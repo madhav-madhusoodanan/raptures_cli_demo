1729ee61e03d4aefd0bb06d1ab7e5fb08750f6b0
sanshrav1311 <92669335+sanshrav1311@users.noreply.github.com>
2023-03-03 17:24:16 +0530

    Update createDao.js

--- scripts/evm/createDao.js
+++ scripts/evm/createDao.js
@@ -1,14 +1,22 @@
 const hre = require("hardhat");
 const { introHandler } = require("../cli-module/intro");
 
-async function createDAO(config) {
+module.exports.createDAO = async (config) => {
+  const TimeLockControl = await hre.ethers.getContractFactory(
+    "TimelockController"
+  );
+  const timeLockControl = await TimeLockControl.deploy(
+    config.minDelay, // uint256
+    config.proposers, // address[]
+    config.executors, // address[]
+    config.admin // address
+  );
+  const Token = await hre.ethers.getContractFactory("Token");
   // config.name, config, contract, ...arguments.
-  const daoFactory = await hre.ethers.getContractFactory("Raptures");
-  const Dao = await daoFactory.deploy(config.token, config.timeLock);
+  const daoFactory = await hre.ethers.getContractFactory("RapturesFactory");
+  const Dao = await daoFactory.deploy(config.token, timeLockControl.address);
 
   // TODO: create dao
   const dao = Dao.connect(introHandler(functionObject));
   return dao;
-}
-
-module.exports = createDAO;
+};
