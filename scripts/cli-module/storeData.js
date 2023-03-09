fb7acf4062435fa8571281252d1d9282a5c9a678
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2023-03-09 17:50:20 +0530

    local data management

--- scripts/cli-module/storeData.js
+++ scripts/cli-module/storeData.js
@@ -1,10 +1,21 @@
-const fs = require('fs/promises');
-import { parse, stringify } from 'yaml'
+const fs = require("fs/promises")
+import { parse, stringify } from "yaml"
 
 module.exports.readData = async () => {
-    return parse(await fs.readFile('data.yaml'))
+    return parse(await fs.readFile("data.yaml"))
 }
 
-module.exports.writeData = async(data) => {
-    await fs.writeFile('data.yaml', stringify(data))
+module.exports.writeData = async (data) => {
+    await fs.writeFile("data.yaml", stringify(data))
+}
+module.exports.addData = async (key, value) => {
+    const oldData = parse(await fs.readFile("data.yaml"))
+    if (oldData?.key) return
+    oldData[key] = value
+    await fs.writeFile("data.yaml", stringify(oldData))
+}
+module.exports.removeData = async (key) => {
+    const oldData = parse(await fs.readFile("data.yaml"))
+    oldData[key] = undefined
+    await fs.writeFile("data.yaml", stringify(oldData))
 }
