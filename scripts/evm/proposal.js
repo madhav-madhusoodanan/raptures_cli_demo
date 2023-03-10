e3e7510a474acf651390e8f99846d6c07484fa83
sanshrav1311 <92669335+sanshrav1311@users.noreply.github.com>
2023-03-10 13:17:50 +0530

    Update proposal.js

--- scripts/evm/proposal.js
+++ scripts/evm/proposal.js
@@ -16,27 +16,14 @@ async function voteOnProposal(config) {
   config.dao.castVote(config.proposalId, config.support);
 }
 
-async function newProposal(config){
-  config.dao.on("ProposalCreated", (proposalId,
-    _msgSender(),
-    targets,
-    values,
-    length, //new string[](targets.length)
-    calldatas,
-    snapshot,
-    deadline,
-    description) => {
-      let proposal = {
-        proposalId: proposalId,
-        _msgSender:_msgSender(),
-        targets:targets,
-        values:values,
-        length:length,//new string[](targets.length),
-        calldatas:calldatas,
-        snapshot:snapshot,
-        deadline:deadline,
-        description:description,
-      }
-      proposalArray.push(proposal);
-    })
+async function listProposal(config) {
+  const filter = {
+    address: config.dao.address,
+    topics: [
+      utils.id(
+        "ProposalCreated(uint256,address,address[],uint256[],string[],bytes[],uint256,uint256,string)"
+      ),
+    ],
+  };
+  proposalArray = config.dao.queryFilter(filter);
 }
