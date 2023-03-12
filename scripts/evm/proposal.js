457e4dd31ef83f927c83506c5c1a41626fd631cf
sanshrav1311 <92669335+sanshrav1311@users.noreply.github.com>
2023-03-12 10:43:45 +0530

    Update proposal.js

--- scripts/evm/proposal.js
+++ scripts/evm/proposal.js
@@ -2,15 +2,25 @@ const hre = require("hardhat");
 
 var proposalArray = [];
 
-async function createProposal(config) {
-  const proposalId = config.dao.propose(
-    config.targets,
-    config.values,
-    config.calldatas,
-    config.description
+module.exports.createProposal = async (config) => {
+  const signers = await hre.ethers.getSigners();
+  const dao = await hre.ethers.getContractAt(
+    "RapturesFactory",
+    "0x1291Be112d480055DaFd8a610b7d1e203891C274",
+    signers[0]
+  );
+  const proposalId = dao.propose(
+    signers.map((signer) => signer.address),
+    [2],
+    [],
+    config.response
+    // config.targets, //address[] memory targets,
+    // config.values, //uint256[] memory values,
+    // config.calldatas, //bytes[] memory calldatas,
+    // config.description //string memory description*/
   );
   return proposalId;
-}
+};
 
 async function voteOnProposal(config) {
   config.dao.castVote(config.proposalId, config.support);
