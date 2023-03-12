a378ad55bbba854dfa721b671fdcbd8f092b94b1
sanshrav1311 <92669335+sanshrav1311@users.noreply.github.com>
2023-03-12 10:41:57 +0530

    Update createProposal.js

--- scripts/cli-module/createProposal.js
+++ scripts/cli-module/createProposal.js
@@ -1,18 +1,20 @@
-const prompts = require("prompts")
+const prompts = require("prompts");
+const { createProposal } = require("../evm/proposal");
+const { readData } = require("./storeData");
 
-const proposalCreater = [
-    {
-        type: "text",
-        name: "title",
-        message: "Add a proposal title",
-    },
-    {
-        type: "text",
-        name: "proposal",
-        message: "Suggest the proposal",
-    },
-]
 module.exports.proposalCreation = async () => {
-    const response = await prompts(proposalCreater)
-    return response
-}
+  const proposalCreater = [
+    // {
+    //   type: "text",
+    //   name: "title",
+    //   message: "Add a proposal title",
+    // },
+    {
+      type: "text",
+      name: "proposal",
+      message: "Suggest the proposal",
+    },
+  ];
+  const response = await prompts(proposalCreater);
+  return createProposal({ Dao: readData, response: response });
+};
