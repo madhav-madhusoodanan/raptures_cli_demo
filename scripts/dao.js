c348a41a23b433f0867dc7149e151f81cd74b7bd
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2023-03-17 04:03:45 +0530

    seamless multi-chain

--- scripts/dao.js
+++ scripts/dao.js
@@ -1,38 +1,39 @@
-const hre = require("hardhat");
-const { createWallet } = require("./evm/createWallet");
-const { createDAO } = require("./evm/createDao");
+const hre = require("hardhat")
+const { createWallet: createWalletEVM } = require("./evm/createWallet")
+const { createDAO: createDAOEVM } = require("./evm/createDao")
 
-const { introHandler } = require("./cli-module/intro");
-const { afterQuestions } = require("./cli-module/afterQuestions.js");
-const { handleDAOCreation } = require("./cli-module/daoCreation");
-const { proposalCreation } = require("./cli-module/createProposal");
-const { voteProposal } = require("./cli-module/voteProposal");
+const { createWallet: createWalletSolana } = require("./solana/createWallet")
 
-/* const response = await prompts({
-    type: "number",
-    name: "value",
-    message: "How old are you?",
-    validate: (value) => (value < 18 ? `Nightclub is 18+ only` : true),
-}) */
+const { introHandler } = require("./cli-module/intro")
+const { afterQuestions } = require("./cli-module/afterQuestions.js")
+const { handleDAOCreation } = require("./cli-module/daoCreation")
+const { proposalCreation } = require("./cli-module/createProposal")
+const { voteProposal } = require("./cli-module/voteProposal")
 
 async function main() {
-  /* 
+    /* 
     handle solana or evm logic here
   */
 
-  const functionObject = {
-    createWallet,
-    handleDAOCreation,
-    proposalCreation,
-    voteProposal,
-    temp: {},
-  };
+    const functionObject = {
+        handleDAOCreation,
+        proposalCreation,
+        voteProposal,
+        temp: {},
+    }
+    if (config) {
+        // evm
+        functionObject.createWallet = createWalletEVM
+    } else {
+        // solana
+        functionObject.createWallet = createWalletSolana
+    }
 
-  const wallet = await introHandler(functionObject);
-  const nextActionFunction = await afterQuestions(functionObject);
-  const actionResponse = await nextActionFunction();
-  console.log(actionResponse);
-  /* 
+    const wallet = await introHandler(functionObject)
+    const nextActionFunction = await afterQuestions(functionObject)
+    const actionResponse = await nextActionFunction()
+    console.log(actionResponse)
+    /* 
   actionResponse will have the set of options that the user chose
   along with the associated function executor
   
@@ -43,11 +44,11 @@ async function main() {
     [name, addresss, etc, ...arguments]
   }
   */
-  const response = actionResponse.value.func(actionResponse.value);
-  console.log(await response);
+    const response = actionResponse.value.func(actionResponse.value)
+    console.log(await response)
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+    console.error(error)
+    process.exitCode = 1
+})
